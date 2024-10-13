b329762454acb6630f261292af723092bf3f7100
Rajat Jain <mznrajatjain@gmail.com>
2024-10-13 11:17:48 +0530

    Adding new member code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from crud import add_book, get_book
+from crud import add_book, get_book, add_member
 
 def addNewBook():
     title = input("Enter book title: ")
@@ -6,6 +6,7 @@ def addNewBook():
     isbn = input("Enter book ISBN: ")
     count = int(input("Enter number of copies: "))
     add_book(title, author, isbn, count)
+    print("> New Book added")
 
 def printBooks():
     books = get_book()
@@ -18,11 +19,21 @@ def printBooks():
         available = "Available" if book.count > 0 else "Not Available"
         print(f"{book.id}: '{book.title}' by {book.author} (ISBN: {book.isbn}) - {available} ({book.count} copies)")
 
+def addNewMember():
+    name = input("Enter member name: ")
+    email = input("Enter member email: ")
+    add_member(name, email)
+    print("> New member added")
 
 def main():
     print("***************************************")
     print("1. Add Book")
     print("2. View Books")
+    print("3. Add Member")
+    print("4. View Member")
+    print("5. Issue Book")
+    print("6. Return Book")
+    print("7. View Transactions by Member")
     print("***************************************")
     choice = input("Enter your choice: ")
 
@@ -30,6 +41,16 @@ def main():
         addNewBook()
     elif choice == "2":
         printBooks()
+    elif choice == "3":
+        addNewMember()
+    elif choice == "4":
+        pass
+    elif choice == "5":
+        pass
+    elif choice == "6":
+        pass
+    elif choice == "7":
+        pass
         
 if __name__ == "__main__":
     main()
